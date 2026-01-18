dc528a8abcfa34723ba3364c26809cefa951ea5e
Ridhi-anil <ridhianilpnr@gmail.com>
2026-01-18 16:16:28 +0530

    events

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Mainb from "./components/Mainb/Mainb.jsx";
+import Events from "./components/Events/Events.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <Mainb />
+      <Events />
     </>
   );
 }
---
+++ src/components/Events/Events.jsx
@@ -0,0 +1,15 @@
+import "./events.css";
+
+export default function Events() {
+  return (
+    <>
+      <div className="event-heading">
+        <h2>Events at Our Club</h2>
+        <p>
+          Join us for exciting events, hands-on workshops, and tech talks !!
+        </p>
+      </div>
+      <div className="events"></div>
+    </>
+  );
+}
