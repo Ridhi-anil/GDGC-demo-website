c14e45dd6281eedb6bc0b20d21d1657166112dce
Ridhi-anil <ridhianilpnr@gmail.com>
2026-01-18 12:32:39 +0530

    Main body

--- src/components/Mainb/Mainb.jsx
+++ src/components/Mainb/Mainb.jsx
@@ -2,9 +2,17 @@ import "./Mainb.css";
 
 export default function Mainb() {
   return (
-    <div className="main-heading">
-      <p>This is the first paragraph.</p>
-      <p>This is the second paragraph.</p>
-    </div>
+    <>
+      <div className="main-heading">
+        <h1>Google Develpers Group MEC</h1>
+        <p>
+          Welcome to the Google Developer Student Club at MEC A community where
+          students learn, collaborate, and build impactful tech
+        </p>
+      </div>
+      <div className="sign-up">
+        <button>Sign Up</button>
+      </div>
+    </>
   );
 }
