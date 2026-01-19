3f3663205b4b726e80394cf98720420c0faf5472
Ridhi-anil <ridhianilpnr@gmail.com>
2026-01-19 20:35:38 +0530

    poster-card

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,4 +1,5 @@
 import "./events.css";
+import eventLogo from "../../assets/techsprintjpeg.jpeg";
 
 export default function Events() {
   return (
@@ -9,7 +10,25 @@ export default function Events() {
           Join us for exciting events, hands-on workshops, and tech talks !!
         </p>
       </div>
-      <div className="events"></div>
+
+      <div className="events">
+        <p id="event-desc">Ongoing Events</p>
+        <div className="event-grid">
+          <div className="event-poster">
+            <img src={eventLogo} />
+            <p id="desc">
+              Tech Sprint a 7 Day online hackathon where you explore and
+              formulate solutions
+            </p>
+            <div className="event-buttons">
+              <button id="more">More</button>
+              <button id="reg">Register</button>
+            </div>
+          </div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
     </>
   );
 }
