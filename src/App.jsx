3529c7b170351c47308ea2db60eb0d834c3950f2
Ridhi-anil <ridhianilpnr@gmail.com>
2026-01-20 17:51:09 +0530

    footer-ready

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Mainb from "./components/Mainb/Mainb.jsx";
 import Events from "./components/Events/Events.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Mainb />
       <Events />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import "./footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-content">
+        <p>About Us</p>
+        <p>
+          Google Developer Student Club at MEC is a community of students
+          passionate about technology, learning, and innovation. We organize
+          workshops, events, and hands-on sessions to help students grow and
+          build together
+        </p>
+        <p>Contact Us</p>
+        <p>Lorem Ipsum : +91 XXXXXXXX</p>
+      </div>
+    </div>
+  );
+}
